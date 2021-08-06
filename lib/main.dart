bc5290c89e73eb9c0d2e78bbfefbca3a3542482c
Henrique Arthur <hnrq.art@gmail.com>
2021-08-05 21:48:49 -0300

    Refactored funds explorer datasource - small improvements

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
-import 'package:dio/dio.dart';
+import 'package:fii_app/shared/interfaces/http_service_interface.dart';
 import 'package:fii_app/shared/interfaces/navigator_service_interface.dart';
 import 'package:fii_app/shared/repositories/reit_repository.dart';
+import 'package:fii_app/shared/services/app_http_service.dart';
 import 'package:fii_app/shared/services/app_navigator_service.dart';
 import 'package:fii_app/shared/stores/reit_store.dart';
 import 'package:flutter/material.dart';
@@ -10,11 +11,14 @@ import 'routes.dart';
 
 void main() {
   GetIt.I.registerLazySingleton(() => ReitStore());
-  GetIt.I.registerLazySingleton(() => Dio());
   GetIt.I.registerLazySingleton(() => ReitRepository());
-  
+
   GetIt.I.registerLazySingleton<NavigatorService>(
     () => AppNavigatorService(),
+  );
+
+  GetIt.I.registerLazySingleton<HttpService>(
+    () => AppHttpService(),
   );
 
   runApp(MyApp());
---
+++ lib/shared/exceptions/datasources/no_table_ranking_exception.dart
@@ -0,0 +1,1 @@
+class NoTableRankingException implements Exception {}
