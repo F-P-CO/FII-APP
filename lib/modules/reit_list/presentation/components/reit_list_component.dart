01025c5e62599738973572c917d1ad9010ac89de
Henrique Arthur <hnrq.art@gmail.com>
2021-08-16 13:40:24 -0300

    Removed unnecessary imports

--- lib/modules/reit_list/presentation/components/reit_list_component.dart
+++ lib/modules/reit_list/presentation/components/reit_list_component.dart
@@ -1,11 +1,9 @@
 import 'package:fii_app/core/domain/entities/reit.dart';
-import 'package:fii_app/core/presentation/themes/app_colors.dart';
 import 'package:fii_app/core/presentation/themes/app_text_styles.dart';
 import 'package:fii_app/modules/reit_list/presentation/stores/reit_list_store.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:get_it/get_it.dart';
-import 'package:shimmer/shimmer.dart';
 
 import 'reit_card_component.dart';
 import 'reit_shimmer_card_component.dart';
